42ed32bb63ea15bb2cc85d08af94c62b8a3433aa
Yathish P Gowda <148539047+Yathish-P-Gowda@users.noreply.github.com>
2024-02-17 22:33:59 +0530

    Update urls.py

--- advapp/urls.py
+++ advapp/urls.py
@@ -6,5 +6,8 @@ app_name ='advapp'
 
 urlpatterns = [
   path('',views.SchoolListView.as_view(),name='list'),
-  path('<int:pk>/',views.SchoolDetailView.as_view(),name='detail'),  
+  path('<int:pk>/',views.SchoolDetailView.as_view(),name='detail'),  #int pk automatically takes a primary key 
+  path('create',views.SchoolCreateView.as_view(),name='create'),
+  path('<int:pk>/update/', views.SchollUpdateView.as_view(), name='update'),
+  path('<int:pk>/delete/', views.SchoolDelete_view.as_view(), name='delete'),
 ] 
